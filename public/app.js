1cb4c2454c06a604aacd99eb802c9a3de60bd3df
Valentine S <dizel3d@gmail.com>
2014-04-10 03:33:53 +0400

    Animation

--- public/app.js
+++ public/app.js
@@ -1,12 +1,17 @@
-angular.module('app',[])
-    .controller('Page1Controller', ['$scope', '$http', function($scope, $http) {
+angular.module('app', ['ngAnimate'])
+    .controller('Page1Controller', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+
+        var nextIsVisible = true;
+        var nextIsVisibleTimeout;
 
         $scope.moveNext = function() {
             $scope.setIndex($scope.index + 1);
+            $scope.moveDirection = '';
         };
 
         $scope.movePrev = function() {
             $scope.setIndex($scope.index - 1);
+            $scope.moveDirection = 'up';
         };
 
         $scope.setIndex = function(index) {
@@ -23,13 +28,33 @@ angular.module('app',[])
                 canMoveNext = false;
             }
 
-            this.index = index;
-            this.canMoveNext = canMoveNext;
-            this.canMovePrev = canMovePrev;
+            this.prevIndex = this.index === undefined ? index : this.index;
+
+            $timeout.cancel(nextIsVisibleTimeout);
+            nextIsVisibleTimeout = $timeout(function() {
+                nextIsVisible = false;
+                $scope.index = index;
+                $scope.moveCount += 1;
+                $scope.canMoveNext = canMoveNext;
+                $scope.canMovePrev = canMovePrev;
+            }, 100); // TODO img.onload
+        };
+
+        $scope.canShowNext = function() {
+            if (!nextIsVisible) {
+                nextIsVisibleTimeout = $timeout(function() {
+                    $timeout.cancel(nextIsVisibleTimeout);
+                    nextIsVisibleTimeout = null;
+
+                    nextIsVisible = true;
+                }, 0);
+            }
+            return nextIsVisible;
         };
 
         $http.get('/data/list.json').success(function(data) {
             $scope.data = data;
+            $scope.moveCount = -1;
             $scope.setIndex(0);
         });
     }]);
