1ecdb5331384b95ac5e698933e124e44ab709fcd
Valentine S <dizel3d@gmail.com>
2014-04-10 00:48:57 +0400

    Slider

--- public/app.js
+++ public/app.js
@@ -1,7 +1,35 @@
 angular.module('app',[])
     .controller('Page1Controller', ['$scope', '$http', function($scope, $http) {
+
+        $scope.moveNext = function() {
+            $scope.setIndex($scope.index + 1);
+        };
+
+        $scope.movePrev = function() {
+            $scope.setIndex($scope.index - 1);
+        };
+
+        $scope.setIndex = function(index) {
+            var indexSup = $scope.data.length - 1;
+            var canMoveNext = true;
+            var canMovePrev = true;
+
+            if (index <= 0) {
+                index = 0;
+                canMovePrev = false;
+            }
+            if (index >= indexSup) {
+                index = indexSup;
+                canMoveNext = false;
+            }
+
+            this.index = index;
+            this.canMoveNext = canMoveNext;
+            this.canMovePrev = canMovePrev;
+        };
+
         $http.get('/data/list.json').success(function(data) {
             $scope.data = data;
+            $scope.setIndex(0);
         });
-        $scope.index = 0;
     }]);
