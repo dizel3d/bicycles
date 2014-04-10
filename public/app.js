b3ed11258fe000714cba0526ccc96b14055be8c2
Valentine S <dizel3d@gmail.com>
2014-04-10 07:39:38 +0400

    Optimization

--- public/app.js
+++ public/app.js
@@ -2,7 +2,6 @@ angular.module('app', ['ngAnimate'])
     .controller('Page1Controller', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
         var nextIsVisible = true;
-        var nextIsVisibleTimeout;
 
         $scope.moveNext = function() {
             $scope.setIndex($scope.index + 1);
@@ -30,22 +29,19 @@ angular.module('app', ['ngAnimate'])
 
             this.prevIndex = this.index === undefined ? index : this.index;
 
-            $timeout.cancel(nextIsVisibleTimeout);
-            nextIsVisibleTimeout = $timeout(function() {
+            // TODO replace with img.onload
+            $timeout(function() {
                 nextIsVisible = false;
                 $scope.index = index;
                 $scope.moveCount += 1;
                 $scope.canMoveNext = canMoveNext;
                 $scope.canMovePrev = canMovePrev;
-            }, 100); // TODO img.onload
+            }, 100);
         };
 
         $scope.canShowNext = function() {
             if (!nextIsVisible) {
-                nextIsVisibleTimeout = $timeout(function() {
-                    $timeout.cancel(nextIsVisibleTimeout);
-                    nextIsVisibleTimeout = null;
-
+                $timeout(function() {
                     nextIsVisible = true;
                 }, 0);
             }
